0c38199a05de05258703d0bcd2cceaaa37fe638d
Collin Hintzke <collinhintzke@outlook.com>
2021-07-28 10:46:13 -0700

    added commandsUsed.md and hello.sh

---
+++ commandsUsed.md
@@ -0,0 +1,11 @@
+git clone https://github.com/210726-wvu-net-ext/P0-Collin-Hintzke.git
+cd P0-Collin-Hintzke
+git checkout -b dev-Collin
+touch commandsUsed.md
+code commandsUsed.md
+
+touch Hello.sh
+code Hello.sh
+git add .
+git commit -m 'Added commandsUsed & Hello.sh'
+git push
---
+++ hello.sh
@@ -0,0 +1,10 @@
+echo "Hello World, I was wondering, whats your favorite movie?"
+read movie1
+echo "NICE! Whats your 2nd favorite?"
+read movie2
+echo "Wow I like that movie too! Alright last one, whats your 3rd favorite?"
+read movie3
+echo "I hate that movie."
+echo "The user's favorite movies are $movie1 , $movie2 , and $movie3"
+echo "Thank you for sharing your favorite movies! Goodbye!"
+read end
